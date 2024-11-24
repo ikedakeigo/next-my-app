9f4d0a83a0705cb23fedd8012e18225aa92027d3
silverskg <silverskg555@gmail.com>
2024-11-24 21:13:19 +0900

    【実装】管理画面記事詳細ページ親

---
+++ src/app/admin/posts/new/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+import PostForm from '@/app/_compornents/PostForm'
+import React from 'react'
+
+const NewPost = () => {
+  return (
+    <PostForm isEdit={false}/>
+  )
+}
+
+export default NewPost
