9f195f9c7defa11292ffd51c5f7f071a23e22579
silverskg <silverskg555@gmail.com>
2024-11-24 21:10:25 +0900

    【実装】api取得時にリクエストno

---
+++ src/app/_types/PostListBody.ts
@@ -0,0 +1,12 @@
+import { RequestCategoryBody } from "./RequestCategoryBody";
+
+export type PostListBody = {
+  id: number;
+  title: string;
+  content: string;
+  createdAt: string;
+  postCategories: {
+    category: RequestCategoryBody;
+  }[];
+  thumbnailUrl: string;
+};
