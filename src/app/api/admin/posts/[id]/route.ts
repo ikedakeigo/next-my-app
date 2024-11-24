8ff94256aa3c47db998f43af4065575a79334909
silverskg <silverskg555@gmail.com>
2024-11-24 21:14:41 +0900

    【修正】api PUT DELETメソッドの作成

--- src/app/api/admin/posts/[id]/route.ts
+++ src/app/api/admin/posts/[id]/route.ts
@@ -3,6 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
 
+// 記事詳細取得API
 export const GET = async (request: NextRequest, { params }: { params: { id: string } }) => {
   const { id } = params;
 
@@ -33,6 +34,91 @@ export const GET = async (request: NextRequest, { params }: { params: { id: stri
       {
         status: 200,
       }
+    );
+  } catch (error) {
+    if (error instanceof Error) {
+      return NextResponse.json(
+        {
+          status: error.message,
+        },
+        {
+          status: 400,
+        }
+      );
+    }
+  }
+};
+
+// 記事更新API
+export const PUT = async (request: NextRequest, { params }: { params: { id: string } }) => {
+  const { id } = params;
+  const body = await request.json();
+  const { title, content, thumbnailUrl, categories } = body;
+
+  try {
+    const post = await prisma.post.update({
+      where: {
+        id: parseInt(id),
+      },
+      data: {
+        title,
+        content,
+        thumbnailUrl,
+      },
+    });
+
+    // 記事とカテゴリーの中間テーブルを削除
+    await prisma.postCategory.deleteMany({
+      where: {
+        postId: parseInt(id),
+      },
+    });
+
+    for (const category of categories) {
+      await prisma.postCategory.create({
+        data: {
+          postId: parseInt(id),
+          categoryId: category.id,
+        },
+      });
+    }
+
+    return NextResponse.json(
+      {
+        status: "OK",
+        post,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    if (error instanceof Error) {
+      return NextResponse.json(
+        {
+          status: error.message,
+        },
+        {
+          status: 400,
+        }
+      );
+    }
+  }
+};
+
+// 記事削除のAPI
+export const DELETE = async (request: NextRequest, { params }: { params: { id: string } }) => {
+  const { id } = params;
+  try {
+    await prisma.post.delete({
+      where: {
+        id: parseInt(id),
+      },
+    });
+
+    return NextResponse.json(
+      {
+        status: "OK",
+      },
+      { status: 200 }
     );
   } catch (error) {
     if (error instanceof Error) {
