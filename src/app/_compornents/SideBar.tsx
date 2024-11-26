bb6a64b16dd3248817f800ee780b96b783459789
silverskg <silverskg555@gmail.com>
2024-11-27 00:40:35 +0900

    【修正】記事一覧,カテゴリー一覧リンク配置

--- src/app/_compornents/SideBar.tsx
+++ src/app/_compornents/SideBar.tsx
@@ -6,11 +6,11 @@ const SideBar = () => {
   return (
     <div className="bg-gray-200 w-80 h-screen">
       <ul>
-        <li className="p-4">
-          <Link href="/">記事一覧</Link>
+        <li className="p-4 text-black">
+          <Link href="/admin/posts">記事一覧</Link>
         </li>
-        <li className="p-4">
-          <Link href="/">カテゴリー一覧</Link>
+        <li className="p-4 text-black">
+          <Link href="/admin/categories">カテゴリー一覧</Link>
         </li>
       </ul>
     </div>
