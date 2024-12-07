4cd61aea9d17475aa5d10d4fc6cc5bf428df985d
silverskg <silverskg555@gmail.com>
2024-12-07 20:29:55 +0900

    【修正】更新用関数を作成-型定義未

--- src/app/admin/categories/new/page.tsx
+++ src/app/admin/categories/new/page.tsx
@@ -1,10 +1,29 @@
 "use client";
 import CategoryForm from '@/app/_compornents/CategoryForm';
+import { useRouter } from 'next/navigation';
 import React from 'react'
 
 const NewCategory = () => {
+  const router = useRouter();
+
+  const handleCreate = async(updateData) => {
+    try {
+      const res = await fetch("/api/admin/categories",{
+        method: "POST",
+        body: JSON.stringify(updateData)
+      });
+      if(res.ok) {
+        router.push("/admin/categories");
+        alert("カテゴリーの作成ができました")
+      }
+    } catch (error) {
+      console.log("データ取得エラー：", error);
+      alert("カテゴリーの作成ができませんでした")
+    }
+  }
+
   // 編集画面の場合はisEditをtrueにする
-  return <CategoryForm isEdit={false}/>
+  return <CategoryForm isEdit={false} onCreate={handleCreate}/>
 }
 
 export default NewCategory
