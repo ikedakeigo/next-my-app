2e537551c3e25e8f62f7fafc0fc2340fe0395644
silverskg <silverskg555@gmail.com>
2024-12-09 00:06:05 +0900

    【修正】セレクターの情報をuseStateで管理していた所をuseFormで管理するように変更

--- src/app/_compornents/PostForm.tsx
+++ src/app/_compornents/PostForm.tsx
@@ -4,35 +4,34 @@ import React, { useEffect, useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { RequestPostBody } from "../_types/RequestPostBody";
 import { RequestCategoryBody } from "../_types/RequestCategoryBody";
+import { PostUpdateData } from "../_types/PostUpdateData";
 
 type PostFormProps = {
   handleDelete?: () => void;
+  onUpdate?: (data: PostUpdateData) => void;
+  onCreate?: (data: PostUpdateData) => void;
   post?: RequestPostBody;
   isEdit?: boolean;
 };
 
-type FormValues = {
-  title: string;
-  content: string;
-  thumbnailUrl: string;
-};
-
-const PostForm = ({ handleDelete, post, isEdit }: PostFormProps) => {
-  const {
-    register, // 入力フィールドを登録する
-    handleSubmit, // フォーム送信時の処理を定義する
-  } = useForm<FormValues>();
-
-  const router = useRouter();
-
+const PostForm: React.FC<PostFormProps> = ({ handleDelete, onUpdate, onCreate, post, isEdit }) => {
   // カテゴリーの初期値
   const [categories, setCategories] = useState<RequestCategoryBody[]>([]);
 
+  const {
+    register, // 入力フィールドを登録する
+    handleSubmit, // フォーム送信時の処理を定義する
+    setValue, // 入力フォームの初期値
+  } = useForm<PostUpdateData>({
+    defaultValues: {
+      categories: post?.postCategories.map((c) => c.category.name) || [],
+    },
+  });
+
+  const router = useRouter();
+
   // 選択されたカテゴリー
   const [categoryName, setCategoryName] = useState<string[]>([]);
-
-  // 選択されたカテゴリーのオブジェクト
-  const [selectCategories, setSelectCategories] = useState<RequestCategoryBody[]>([]);
 
   useEffect(() => {
     const fetchCategory = async () => {
@@ -42,73 +41,43 @@ const PostForm = ({ handleDelete, post, isEdit }: PostFormProps) => {
       const categoryData = await allCategories.json();
 
       setCategories(categoryData.categories);
-
       if (post) {
         // カテゴリーのオブジェクトを取得
         const categoryLists = post.postCategories.map((c) => c.category);
+        console.log("カテゴリーのリスト", categoryLists);
 
         // カテゴリー名のリストをカテゴリーの初期値に設定
-        const categoryNames = categoryLists.map((c) => c.name);
+        const categoryNames = categoryLists.filter((c) => c).map((c) => c.name);
+        setValue("categories", categoryNames);
         setCategoryName(categoryNames);
+        console.log("初期値のカテゴリー", categoryNames);
       }
     };
 
     fetchCategory();
   }, [post]);
 
-  const handleChange = (edit: React.ChangeEvent<HTMLSelectElement>) => {
-    const {
-      target: { value },
-    } = edit;
-
-    // 選択されたカテゴリー名を更新
-    const selectedCategoryNames = typeof value === "string" ? value.split(",") : value;
-    setCategoryName(selectedCategoryNames);
-
-    // 選択されたカテゴリー名に基づいて、selectCategoryを更新
-    // filterメソッドを使用して、選択されたカテゴリー名に一致するカテゴリーを取得
-    /**
-     * filterメソッドの書き方
-     * - ブラケット `{}` を使用する場合、`return` を明示的に記述する必要がある。
-     * - 簡潔に書く場合、括弧 `()` を使用することで暗黙的に値を返せる。
-     *
-     * 例:
-     * const updatedSelectedCategories = categories.filter((c) => (
-     *   selectedCategoryNames.includes(c.name)
-     * ));
-     */
-    // return が必要な場合
-    // const updatedSelectedCategories = categories.filter((c) => {
-    //  return selectedCategoryNames.includes(c.name);
-    // });
-    // 省略記法 以下
-    const updatedSelectedCategories = categories.filter((c) => selectedCategoryNames.includes(c.name));
-
-    console.log("updatedSelectedCategories------->", updatedSelectedCategories);
-    setSelectCategories(updatedSelectedCategories);
-  };
-
   // 記事投稿
-  const onsubmit: SubmitHandler<FormValues> = async (data) => {
+  const onsubmit: SubmitHandler<PostUpdateData> = async (data) => {
+    const initCategories = categories
+    //includesを使用してその名前に一致するカテゴリーのidを取得
+    // @ts-ignore
+    .filter((category) => data.categories.includes(category.name))
+    //フィルタリングしたカテゴリーからidを取り出し、新しいオブジェクトに変換
+    .map((category) => ({ id: category.id }));
+
     const updateData = {
       ...data,
-      categories: selectCategories,
+      categories: initCategories
     };
 
-    console.log("updateData------->", updateData);
+    console.log("更新する記事のカテゴリー", categoryName);
 
-    try {
-      const res = await fetch(`/api/admin/posts/${isEdit ? `${post?.id}` : ""}`, {
-        method: isEdit ? "PUT" : "POST",
-        body: JSON.stringify(updateData),
-      });
-      if (res.ok) {
-        router.push("/admin/posts");
-        alert(isEdit ? "記事を更新しました" : "記事を作成しました");
-      }
-    } catch (error) {
-      console.log(error);
-      alert(isEdit ? "記事の更新に失敗しました" : "");
+    if (post?.id) {
+      console.log("フォームのカテゴリー", updateData);
+      onUpdate?.(updateData);
+    } else {
+      onCreate?.(updateData);
     }
   };
 
@@ -141,7 +110,17 @@ const PostForm = ({ handleDelete, post, isEdit }: PostFormProps) => {
           <label htmlFor="categories" className="mb-2 text-sm font-medium text-gray-700">
             カテゴリー
           </label>
-          <select id="categories" multiple value={categoryName} onChange={handleChange} className="border text-blue-500  border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500">
+          <select
+            id="categories"
+            multiple
+            {...register("categories")}
+            onChange={(e) => {
+              const selectedOptions = Array.from(e.target.selectedOptions).map((option) => option.value);
+              // useForm の状態を更新
+              setValue("categories", selectedOptions);
+            }}
+            className="border text-blue-500  border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          >
             {categories.map((category) => (
               <option key={category.id} value={category.name}>
                 {category.name}
@@ -172,6 +151,7 @@ const PostForm = ({ handleDelete, post, isEdit }: PostFormProps) => {
           </button>
         </div>
       </form>
+
     </div>
   );
 };
