99d8908fb49e0663f7d1cc18a43e552c06101f8d
silverskg <silverskg555@gmail.com>
2025-01-08 01:01:48 +0900

    ホスト名の変更

--- next.config.mjs
+++ next.config.mjs
@@ -3,16 +3,10 @@ const nextConfig = {
   images: {
     // domains: ['placeimg.com'],
     remotePatterns: [
-      // {
-      //   protocol: "https",
-      //   hostname: "images.microcms-assets.io",
-      // },
-      {
-        protocol: "https",
-        hostname: "placehold.jp", // 開発環境用
-        // port: "3000", // 開発環境用
-        // pathname: "/api/**", // API配下のすべての画像を許可
-      },
+      { protocol: "https", hostname: "placehold.jp" },
+      { protocol: "https", hostname: "images.microcms-assets.io" },
+      { protocol: "http", hostname: "example.com" },
+      { protocol: "https", hostname: "vkxmusukhyewhjxghhpt.supabase.co" },
     ],
   },
 };
