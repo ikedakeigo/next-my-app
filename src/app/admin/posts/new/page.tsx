c842d3b5e02c1eba75dfa7b6e2c23cd8c58d71d9
silverskg <silverskg555@gmail.com>
2025-01-08 23:06:02 +0900

    【追加】sessionのtokenを取得、useStateで画像の情報を管理,PostFormに画像の情報を渡す処理

--- src/app/admin/posts/new/page.tsx
+++ src/app/admin/posts/new/page.tsx
@@ -1,28 +1,35 @@
 "use client";
 import PostForm from "@/app/_compornents/PostForm";
+import { useSupabaseSession } from "@/app/_hook/useSupabaseSession";
 import { PostUpdateData } from "@/app/_types/PostUpdateData";
 import { useRouter } from "next/navigation";
-import React from "react";
+import React, { useState } from "react";
 
 const NewPost = () => {
   const router = useRouter();
-
-  const handleCreate = async(updateData: PostUpdateData) => {
+  const [thumbnailImageKey, setThumbnailImageKey] = useState("");
+  const { token } = useSupabaseSession();
+  const handleCreate = async (updateData: PostUpdateData) => {
+    if (!token) return;
     try {
       const res = await fetch("/api/admin/posts", {
         method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: token, // Header に token を付与
+        },
         body: JSON.stringify(updateData),
       });
-      if(res.ok){
+      if (res.ok) {
         router.push("/admin/posts");
-        alert("記事の作成ができました")
+        alert("記事の作成ができました");
       }
     } catch (error) {
       console.log("データ取得エラー：", error);
-      alert("記事の作成ができませんでした")
+      alert("記事の作成ができませんでした");
     }
-  }
-  return <PostForm isEdit={false} onCreate={handleCreate} />;
+  };
+  return <PostForm isEdit={false} onCreate={handleCreate} thumbnailImageKey={thumbnailImageKey} setThumbnailImageKey={setThumbnailImageKey} />;
 };
 
 export default NewPost;
